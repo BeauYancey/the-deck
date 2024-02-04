d6ed87dd7882a350c0c3202a61be4b385f873dc8
Beau Yancey <yancey.beau.j@gmail.com>
2024-02-04 00:34:45 -0700

    added mock events page

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,20 +2,37 @@ import { useState, useEffect } from "react";
 
 function Home() {
 
-  const [joke, setJoke] = useState("");
-  useEffect(() => {
-    fetch("https://icanhazdadjoke.com/", {headers: {"Accept": "application/json"}})
-    .then(res => res.json())
-    .then(data => setJoke(data.joke))
-  }, []);
+  // const [events, setEvents] = useState([]);
+  // useEffect(() => {
+  //   fetch("/api/events")
+  //   .then(res => res.json())
+  //   .then(data => setEvents(data))
+  // });
+
+  const events = [
+    {date: new Date(2024, 1, 5), name: "Family Night", description: "Bring your kids"},
+    {date: new Date(2024, 1, 8), name: "Ladies Night", description: "I'm not invited :("},
+    {date: new Date(2024, 1, 10), name: "DnD Tourney", description: "Goblins and ghouls. IDK I've never played DnD"}
+  ];
+  const dateDisplay = { weekday: 'long', month: 'short', day: 'numeric' };
 
   return (
     <div>
       <img className="banner-image" alt="people playing games" src="https://images.pexels.com/photos/8111307/pexels-photo-8111307.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" />
       <div className="home-content">
-        <h1>Home Page</h1>
-        <p>I'm not sure what will go on this page, but I feel like the website needs a home page. It can't just go straight to the games.</p>
-        <p>{joke}</p>
+        <h1 style={{paddingBottom: ".5em"}}>Upcoming Events</h1>
+        <div className="upcoming-events">
+          {events.map(event => (
+            <div style={{padding: "1em 0 1em .5em", borderTop: "solid 2px grey", display: "flex"}}>
+              <img style={{height: "8em", width: "8em", objectFit: "cover"}} src="https://media.npr.org/assets/img/2022/11/04/gettyimages-1183414292-1-_slide-edff8c3fe6afcab5c6457e3c7bd011f5c1745161.jpg" />
+              <div style={{paddingLeft: "1em"}}>
+                <h2>{event.name}</h2>
+                <h4>{event.date.toLocaleDateString('en-us', dateDisplay)}</h4>
+                <p>{event.description}</p>
+              </div>
+            </div>
+          ))}    
+        </div>
       </div>
     </div>
   );
